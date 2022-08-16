c100bf3ad06f3ce051275e4c7d40f9603b44f3c8
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-08-16 11:46:31 +0200

    Add project files.

---
+++ Piskvorky/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Piskvorky
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Piskvorky/Program.cs
@@ -0,0 +1,15 @@
+namespace Piskvorky
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
